63d711ebf704377fe5cb1f14eb17457835016c2c
MrC0de <neurosys@neuroticnetworks.com>
2016-11-06 00:06:38 -0700

    openGlTest application

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@ using namespace std;
 int main(int argc, char** argv) {
     cout<<"*** openGLTest Started ***"<<endl;
     // Go
-     SDL_Init(SDL_INIT_EVERYTHING);
-
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Delay(1000);
-
     SDL_Quit();
     // Stop
     cout<<"*** openGLTest Finished ***"<<endl;
