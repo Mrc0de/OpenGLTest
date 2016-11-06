c4cf4c7c160a0af0fe7f11a52270345a8f6bb3b4
MrC0de <neurosys@neuroticnetworks.com>
2016-11-06 00:15:14 -0700

    openGlTest application

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_opengl.h>
 using namespace std;
 
 int main(int argc, char** argv) {
     cout<<"*** openGLTest Started ***"<<endl;
     // Go
     SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_Window* window = SDL_CreateWindow("OpenGL It Works...", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+    //SDL_WINDOW_OPENGL - Create a window ready for OpenGL.
+    //SDL_WINDOW_RESIZABLE - Create a resizable window.
+    //SDL_WINDOW_FULLSCREEN - Create a fullscreen window.
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    
+    SDL_Event windowEvent;
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) { 
+            if (windowEvent.type == SDL_QUIT) break;
+            //if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+        }
+        SDL_GL_SwapWindow(window);
+    }
+    
     SDL_Delay(1000);
+    SDL_GL_DeleteContext(context);
     SDL_Quit();
     // Stop
     cout<<"*** openGLTest Finished ***"<<endl;
