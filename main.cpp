f6e7e25bbc5cc0f1a425d0439ea9b411a67f9406
MrC0de <neurosys@neuroticnetworks.com>
2016-11-06 14:18:04 -0800

    openGlTest application, pretty triangles

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+// Shader sources
+const GLchar* vertexSource =
+    "#version 150 core\n"
+    "in vec2 position;"
+    "void main()"
+    "{"
+    "    gl_Position = vec4(position, 0.0, 1.0);"
+    "}";
+const GLchar* fragmentSource =
+    "#version 150 core\n"
+    "out vec4 outColor;"
+    "void main()"
+    "{"
+    "    outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+    "}";
+
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -11,7 +27,7 @@ int main(int argc, char** argv) {
     // Go
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_Window* window = SDL_CreateWindow("OpenGL It Works...", 0, 0, 800, 600, SDL_WINDOW_OPENGL);
@@ -19,21 +35,71 @@ int main(int argc, char** argv) {
     //SDL_WINDOW_RESIZABLE - Create a resizable window.
     //SDL_WINDOW_FULLSCREEN - Create a fullscreen window.
     SDL_GLContext context = SDL_GL_CreateContext(window);
+    
     glewExperimental = GL_TRUE;
     glewInit();
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
     printf("%u\n", vertexBuffer);
+    
+    GLfloat vertices[] = {
+        0.0f,  0.5f, 
+        0.5f, -0.5f, 
+        -0.5f, -0.5f 
+    };
+    
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    GLuint vbo;
+    glGenBuffers(1, &vbo); 
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLint status;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    cout<<"Shader Compile Status For VertexShader: "<<status<<endl;
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    cout<<"Shader Compile Status For FragmentShader: "<<status<<endl;
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(posAttrib);
+
     SDL_Event windowEvent;
     while (true) {
         if (SDL_PollEvent(&windowEvent)) { 
-            if (windowEvent.type == SDL_QUIT) break;
-            if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+            if (windowEvent.type == SDL_QUIT) {break;}
+            if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE) {break;}
         }
-        //SDL_GL_SwapWindow(window);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(window);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
-    
-    SDL_Delay(1000);
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
     SDL_GL_DeleteContext(context);
     SDL_Quit();
     // Stop
